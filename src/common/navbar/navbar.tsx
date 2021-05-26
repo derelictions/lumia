0eb1fe6ee25c7fbe139c46ec8fa41dcd00309196
derelict <derelictions@protonmail.com>
2021-05-26 17:03:13 -0400

    some more few tweaks

--- src/common/navbar/navbar.tsx
+++ src/common/navbar/navbar.tsx
@@ -1,12 +1,17 @@
-import router from "next/router";
-import React, { Props } from "react";
-import { AppProps } from "next/app";
-import { Box, Text, Image, Stack, Link } from "@chakra-ui/react";
-import { CloseIcon, TriangleDownIcon, SunIcon, MoonIcon } from "@chakra-ui/icons";
+import router from 'next/router';
+import React, { Props } from 'react';
+import { AppProps } from 'next/app';
+import { Box, Text, Image, Stack, HStack, Link } from '@chakra-ui/react';
+import {
+	CloseIcon,
+	TriangleDownIcon,
+	SunIcon,
+	MoonIcon,
+} from '@chakra-ui/icons';
 
 // const Navbar: React.FC<{}> = () => {
 //     const router = useRouter();
-    
+
 //     return (
 //         <>
 //             <Flex justify="space-between" m={4}>
@@ -16,62 +21,67 @@ import { CloseIcon, TriangleDownIcon, SunIcon, MoonIcon } from "@chakra-ui/icons
 //             </Flex>
 //         </>
 //     )
-    
+
 // }
+
+export default function Navbar({}) {
+	return (
+		<HStack>
+			<Logo />
+		</HStack>
+	);
+}
 
 // Logo component
 
-function Logo({pageProps}: AppProps) {
-    return (
-        <Box {... pageProps}>
-            <Image src="../../../logo.png" />
-            <Text fontSize="lg" fontWeight="bold" >
-                Lumia
-            </Text>
-        </Box>
-    ) 
+function Logo({}) {
+	return (
+		<HStack>
+			<Image w='50px' src='/altl.png' alt='Lumia' />
+			<Text fontSize='lg' fontWeight='bold'>
+				Lumia
+			</Text>
+		</HStack>
+	);
 }
 
 // Drop down button for toggling menu on phone and tablet
 
-function MenuToggle({toggle, isOpen}: any) {
-    return (
-        <Box display={{base: "block", md: "none"}} onClick={toggle}>
-            {isOpen ? <CloseIcon/> : <TriangleDownIcon/>}
-        </Box>
-    )
+function MenuToggle({ toggle, isOpen }: any) {
+	return (
+		<Box display={{ base: 'block', md: 'none' }} onClick={toggle}>
+			{isOpen ? <CloseIcon /> : <TriangleDownIcon />}
+		</Box>
+	);
 }
 
 // For dark and light theme
 
-function ThemeToggle({toggle, isOpen}: any) {
-    return (
-        <Box display={{base: "block", md: "none"}} onClick={toggle}>
-            {isOpen ? <MoonIcon/> : <SunIcon/>}
-        </Box>
-    )
+function ThemeToggle({ toggle, isOpen }: any) {
+	return (
+		<Box display={{ base: 'block', md: 'none' }} onClick={toggle}>
+			{isOpen ? <MoonIcon /> : <SunIcon />}
+		</Box>
+	);
 }
 
-// TODO: 
+// TODO: stack depeneding on responsive breakpts
 
 function Header({}) {
-    const [isOpen, setIsOpen] = React.useState(false)
-    const toggle = () => setIsOpen(!isOpen)
-    
-    return (
-        <MenuToggle toggle={toggle} isOpen={isOpen} />
-    )
+	const [isOpen, setIsOpen] = React.useState(false);
+	const toggle = () => setIsOpen(!isOpen);
 
+	return <MenuToggle toggle={toggle} isOpen={isOpen} />;
 }
 
-function MenuItem({children, isLast, to="/", ...cdr}: any) {
-    return (
-        <Link onClick={() => router.push(to)}>
-            <Text display="block" {...cdr}>
-                {children}
-            </Text>
-        </Link>
-    )
+function MenuItem({ children, isLast, to = '/', ...rest }: any) {
+	return (
+		<Link onClick={() => router.push(to)}>
+			<Text display='block' {...rest}>
+				{children}
+			</Text>
+		</Link>
+	);
 }
 
 // We create responsive breakpoints with the Stack component
